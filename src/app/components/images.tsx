35933fd19b103d27f26cf1e61264433572513a93
Pham Luong <phamluong10399@gmail.com>
2025-06-13 22:09:33 +0700

    feat: add filter

--- src/app/components/images.tsx
+++ src/app/components/images.tsx
@@ -29,32 +29,35 @@ const Images = () => {
       </div>
 
       <div className="grid grid-cols-5 max-xl:grid-cols-4 max-sm:grid-cols-2 gap-2 px-[150px] max-sm:px-[5px] max-md:grid-cols-4 max-md:px-[50px]">
-        {Array.from({ length: 49 }, (_, i) => i + 1).map((i) => (
-          <motion.div
-            key={i}
-            whileHover={{
-              scale: 1.05,
-            }}
-            initial={{
-              scale: 0,
-            }}
-            whileInView={{
-              scale: 1,
-            }}
-            transition={{
-              duration: 0.6,
-            }}
-            className="flex items-center justify-center w-[100%] cursor-pointer"
-          >
-            <img
-              src={`/gallery/${i}.jpg`}
-              style={{
-                boxShadow: "box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px",
+        {Array.from({ length: 49 }, (_, i) => i + 1)
+          .filter((x) => ![25, 26, 22, 23, 34].includes(x))
+          .map((i) => (
+            <motion.div
+              key={i}
+              whileHover={{
+                scale: 1.05,
               }}
-              className="h-[270px] w-[180px] rounded-[10px] border-[4px] border-gray-100 object-cover"
-            />
-          </motion.div>
-        ))}
+              initial={{
+                scale: 0,
+              }}
+              whileInView={{
+                scale: 1,
+              }}
+              transition={{
+                duration: 0.6,
+              }}
+              className="flex items-center justify-center w-[100%] cursor-pointer"
+            >
+              <img
+                src={`/gallery/${i}.jpg`}
+                style={{
+                  boxShadow:
+                    "box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px",
+                }}
+                className="h-[270px] w-[180px] rounded-[10px] border-[4px] border-gray-100 object-cover"
+              />
+            </motion.div>
+          ))}
       </div>
     </div>
   );
